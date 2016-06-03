e31e7f7e5491628108c516d4b8078b1701102ddb
fredhgy <fredhgy2010@gmail.com>
2016-06-03 15:10:49 +0800

    更新第二章部分内容，更新关于，菜单内容

--- Learn Matlab UWP/Models/Menu.cs
+++ Learn Matlab UWP/Models/Menu.cs
@@ -9,16 +9,16 @@ namespace Learn_Matlab_UWP.Models
         {
             MenuItems = new List<MenuItem>();
             MenuItems.Add(new MenuItem("\uE1CE", "首页", typeof(StartView)));
-            MenuItems.Add(new MenuItem("\uE1CE", "第一章", typeof(Ch1View)));
-            MenuItems.Add(new MenuItem("\uE1CE", "第二章", typeof(Ch2View)));
-            MenuItems.Add(new MenuItem("\uE1CE", "第三章", typeof(Ch3View)));
-            MenuItems.Add(new MenuItem("\uE1CE", "第四章", typeof(Ch4View)));
-            MenuItems.Add(new MenuItem("\uE1CE", "第五章", typeof(Ch5View)));
+            MenuItems.Add(new MenuItem("\uE1CE", "第一章 入门", typeof(Ch1View)));
+            MenuItems.Add(new MenuItem("\uE1CE", "第二章 符号计算", typeof(Ch2View)));
+            MenuItems.Add(new MenuItem("\uE1CE", "第三章 数组运算", typeof(Ch3View)));
+            MenuItems.Add(new MenuItem("\uE1CE", "第四章 数值运算", typeof(Ch4View)));
+            MenuItems.Add(new MenuItem("\uE1CE", "第五章 可视化", typeof(Ch5View)));
             MenuItems.Add(new MenuItem("\uE1CE", "第六章", typeof(Ch6View)));
             MenuItems.Add(new MenuItem("\uE1CE", "第七章", typeof(Ch7View)));
             MenuItems.Add(new MenuItem("\uE1CE", "第八章", typeof(Ch8View)));
             MenuItems.Add(new MenuItem("\uE1CE", "关于", typeof(AboutView)));
-            MenuItems.Add(new MenuItem("\uE1CE", "版本号：1.0.0", typeof(ImportView)));
+            MenuItems.Add(new MenuItem("\uE1CE", "版本号：1.1606.0301", typeof(ImportView)));
         }
     }
 }
